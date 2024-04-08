18477c21ab47181ff158d23431374acf3233c6e9
decamilo <kmj030306@naver.com>
2024-04-08 18:56:24 +0900

    dkdkd

---
+++ index.js
@@ -0,0 +1,18 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+import cors from 'cors';
+
+import { ping } from './controller/system.js';
+
+const serverPort = 3000;
+const app= express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+const router = express.Router();
+
+router.route('/ping').get(ping);
+
+app.use('/socialLoginProject/api/v1',router);
+app.listen(serverPort);
+//http://localhost:3000/socialLoginProject/api/v1/ping
